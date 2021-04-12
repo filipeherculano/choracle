baf1ac640391df814129aa1fce8ffb633586ceb2
Filipe Herculano <filipeherculanorocha@gmail.com>
2021-04-12 13:45:41 -0300

    phx new

---
+++ lib/choracle/application.ex
@@ -0,0 +1,34 @@
+defmodule Choracle.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      Choracle.Repo,
+      # Start the Telemetry supervisor
+      ChoracleWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Choracle.PubSub},
+      # Start the Endpoint (http/https)
+      ChoracleWeb.Endpoint
+      # Start a worker by calling: Choracle.Worker.start_link(arg)
+      # {Choracle.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Choracle.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ChoracleWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
